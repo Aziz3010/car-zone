2590085d5db3b71d911e018377734f5e7601b2e4
unknown <a.abdelazizg@gmail.com>
2022-02-24 02:12:04 +0200

    added js code  and media q

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let burger_btn = document.getElementById('burger-btn');
+let my_links = document.getElementById('my-links');
+
+burger_btn.addEventListener('click',function(){
+    my_links.classList.toggle('on')
+})
